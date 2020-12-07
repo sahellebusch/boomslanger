8cb184abb004559c11341c8bcfa9bc4e69882a31
Sean Hellebusch <sahellebusch@users.noreply.github.com>
2020-12-07 08:48:54 -0800

    feat: add findOne functionality (#3)

--- postgres/initdb.sql
+++ postgres/initdb.sql
@@ -1,12 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE films (
+    uuid        uuid DEFAULT uuid_generate_v4 (),
     id          serial PRIMARY KEY,
     title       varchar(40) NOT NULL,
     date_prod   timestamp with time zone,
     len         integer NOT NULL,
     meta        jsonb
-);
-
-CREATE TABLE distributors (
-     did    serial PRIMARY KEY,
-     name   varchar(40) NOT NULL CHECK (name <> '')
 );
